158872faffb7b9af201edf8176c89cb5866d0da2
HQ01UG982 <hq01ug982@mb.com>
2019-04-29 18:41:22 +0800

    springcloud学习

--- admin/src/main/java/com/tu/admin/AdminApplication.java
+++ admin/src/main/java/com/tu/admin/AdminApplication.java
@@ -2,12 +2,10 @@ package com.tu.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @Description springboot admin 监控启动类
