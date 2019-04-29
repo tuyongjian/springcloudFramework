f3233b0c3fcf9dcbdc6ac1304df9821385046412
HQ01UG982 <hq01ug982@mb.com>
2019-04-29 18:26:36 +0800

    springcloud学习

--- admin/src/main/java/com/tu/admin/AdminApplication.java
+++ admin/src/main/java/com/tu/admin/AdminApplication.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.Configuration;
  *
  */
 
+@EnableDiscoveryClient
 @EnableEurekaClient
 @EnableAdminServer
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
--- webapp/src/main/java/com/tu/webapp/WebAppApplication.java
+++ webapp/src/main/java/com/tu/webapp/WebAppApplication.java
@@ -3,6 +3,7 @@ package com.tu.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Description
@@ -22,6 +23,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class WebAppApplication {
 
     public static void main(String[] args) {
