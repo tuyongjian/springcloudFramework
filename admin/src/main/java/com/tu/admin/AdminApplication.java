bb8b44bbff453b172e51ea04b850157481e5d268
HQ01UG982 <hq01ug982@mb.com>
2019-04-29 18:00:14 +0800

    springcloud学习

--- admin/src/main/java/com/tu/admin/AdminApplication.java
+++ admin/src/main/java/com/tu/admin/AdminApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,9 +20,9 @@ import org.springframework.context.annotation.Configuration;
  *
  */
 
-@EnableAdminServer
 @EnableEurekaClient
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableAdminServer
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class,args);
