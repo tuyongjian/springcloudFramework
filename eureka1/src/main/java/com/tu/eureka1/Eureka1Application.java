f33a3e44fff12af0a0c4069477a7e83c68bb3d93
tuyongjian <1475591137@qq.com>
2019-05-05 20:29:41 +0800

    springcloud springadmin配置学习

---
+++ eureka1/src/main/java/com/tu/eureka1/Eureka1Application.java
@@ -0,0 +1,25 @@
+package com.tu.eureka1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description eureka启动类，eureka是springcloud的服务注册中心
+ * 可以把服务注册到注册中心，进行集中管理
+ *
+ * @Classname EurekaApplication
+ * @Date 2019/4/29 15:11
+ * @Created by tuyongjian
+ *
+ * EnableEurekaServer 注解开启eureka服务
+ */
+@EnableEurekaServer
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class Eureka1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka1Application.class,args);
+    }
+}
