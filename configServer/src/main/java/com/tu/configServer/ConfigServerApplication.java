494615ac3bc9bffa2fbcb4d22c64d1ad360a80f9
HQ01UG982 <hq01ug982@mb.com>
2019-05-27 14:06:04 +0800

    springcloud学习

--- configServer/src/main/java/com/tu/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/com/tu/configServer/ConfigServerApplication.java
@@ -3,11 +3,16 @@ package com.tu.configServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by tuyongjian on 2019/5/20.
+ * EnableConfigServer 开启config服务端
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication{
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class,args);
