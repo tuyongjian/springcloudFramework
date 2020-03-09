f2169853fc6d43f1f6b7290a14fa27d51136e565
HQ01UG982 <hq01ug982@mb.com>
2020-03-09 14:36:23 +0800

    调整pom

--- gateway/src/main/java/com/tu/gateway/GateWayApplication.java
+++ gateway/src/main/java/com/tu/gateway/GateWayApplication.java
@@ -10,6 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @Date: 2020/3/9 10:29
  * @Description: springcloud 提供的新的网关服务
  *
+ * gateway是使用webFlue框架实现的，所以不能引入spring-boot-starter-web包
+ *
  * gateway
  * 参考网址 https://www.cnblogs.com/qianwei/p/10127700.html
  * https://www.cnblogs.com/crazymakercircle/p/11704077.html
