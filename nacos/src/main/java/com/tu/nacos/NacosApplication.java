eba5577fdc342cd9df1eeef7289067581b1e189f
HQ01UG982 <hq01ug982@mb.com>
2020-05-11 14:53:21 +0800

    nacos

---
+++ nacos/src/main/java/com/tu/nacos/NacosApplication.java
@@ -0,0 +1,13 @@
+package com.tu.nacos;
+
+/**
+ * Hello world!
+ *
+ */
+public class NacosApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
